4103eb55a72a0ac660b68510833206967d9457c8
masabarni0902 <55786999+masabarni0902@users.noreply.github.com>
2026-05-18 13:33:41 +0200

    Add files via upload

---
+++ A.Feladat/A.Feladat/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Net;
+using OpenQA.Selenium.Support.UI;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            IWebDriver driver = new ChromeDriver(options);
+
+            try
+            {
+                driver.Navigate().GoToUrl("http://localhost/login.html");
+
+                var nameInput = driver.FindElement(By.Id("name"));
+                nameInput.SendKeys("Teszt Elek");
+
+                var emailInput = driver.FindElement(By.Id("email"));
+                emailInput.SendKeys("tesztelek@gmail.com");
+
+                var pwdInput = driver.FindElement(By.Id("password"));
+                pwdInput.SendKeys("asd123");
+
+                var stayInput = driver.FindElement(By.Id("stay"));
+                stayInput.Click();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hiba Történt: " + ex.Message);
+            }
+            finally
+            {
+                //driver.Quit(); 
+            }
+        }
+    }
+}
